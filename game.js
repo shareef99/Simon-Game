04fac6d5d53bfa5bd65c70ece188589b244e2efe
Mohammed Nadeem Shareef <nadeemshareef934@gmail.com>
2019-07-03 15:57:10 +0530

    Update game.js

--- game.js
+++ game.js
@@ -15,6 +15,14 @@ $(document).keypress(function() {
   }
 });
 
+$(document).click(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
